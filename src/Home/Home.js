88ab22ad3e09e0bf93082529a2859ce3dfd8ec4e
xela <beauquel.alexandre@gmail.com>
2018-02-21 15:10:35 +0100

    first rédaction home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -13,8 +13,17 @@ class Home extends Component{
         return(
             <Container>
 
-                <h3>home</h3>
+                <h3>Portfolio créé par alexandre beauquel</h3>
+                <br/>
+                <p>Ce projet permet de répondre à plusieurs epreuves du BTS SIO</p>
+                <p>Epreuve E4 :  Décrire des situations rencontrées et lien avec les
+                    activités du référentiel (BTS SIO). Chaque situation correspond à une réalisation plus ou moins
+                    indépendante, correspondant à un nombre limité d’activités du référentiel. Le terme de
+                    situation est ici à rapprocher de sa définition lors de l’épreuve E4.</p>
 
+                <p>Epreuve E6 : Evaluation en partie sur la gestion du patrimoine
+                    informatique et sur la veille technologique. Ce projet est donc en mesur de pouvoir fournir des
+                    éléments concrets sur ces points. Montrer les outils et méthodes utilisés et rencontrer personnelement ou en entreprise.</p>
             </Container>
 
         )}
