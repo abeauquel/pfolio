574ac18a20a83c61069d00492c6e3541a62d70f7
abeauquel <31766247+abeauquel@users.noreply.github.com>
2018-04-16 16:40:27 +0200

    tesdte

--- src/E4/Competences.js
+++ src/E4/Competences.js
@@ -197,7 +197,7 @@ class Competences extends Component{
                                     : null}
 
                                 {/** PDF */}
-                                {Illust.Illustration.havePDF === '1' ? <div>
+                                {Illust.Illustration.havePDF == '1' ? <div>
                                       <p><a target="_blank"
                                             href={"/img/illustration/illustration"
                                             + Illust.Illustration.id + ".pdf"}>Télécharger
